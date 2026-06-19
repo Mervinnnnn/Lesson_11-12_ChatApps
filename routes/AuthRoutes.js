643c198e9f38e1b0d593ac0401ea680031663f73
Mervinnnnn <mervinhuang08@gmail.com>
2026-06-19 12:00:23 +0700

    ChatApp

---
+++ routes/AuthRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+const router = express.Router()
+
+import { loginPage, registerPage, registerUser, loginUser, logOutUser, chatPage} from '../controllers/AuthController.js'
+
+
+router.get("/register", registerPage)
+router.get("/login", loginPage)
+router.post('/login', loginUser)
+router.post('/register', registerUser)
+router.get('/logout', logOutUser)
+router.get("/chat", chatPage)
+
+
+export default router
